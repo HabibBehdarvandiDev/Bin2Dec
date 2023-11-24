dd8df519bc5b913af1e21c8b03a6a29e07e2c16a
Habib <habibbehdarvandi.dev@gmail.com>
2023-11-24 05:31:56 -0800

    Create HandleBinary

--- app/Components/form.tsx
+++ app/Components/form.tsx
@@ -29,6 +29,10 @@ function bin2Decimal(num: number) {
     return reserve
 }
 
+const handleBinary = (e: React.ChangeEvent<HTMLInputElement>) => {
+    
+}
+
 
 
 const form = () => {
@@ -46,6 +50,7 @@ const form = () => {
                     name="binary"
                     id="binaryInput"
                     className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    onChange={(e) => handleBinary(e)}
                 />
             </div>
             <div className='flex flex-col my-4'>
