ac53c1cc413672cf6eb479eca2ac8beba5208e0c
Habib <habibbehdarvandi.dev@gmail.com>
2023-11-24 08:48:51 -0800

    Final v.1.1

--- app/Components/form.tsx
+++ app/Components/form.tsx
@@ -1,40 +1,37 @@
-"use client";
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-function dec2Binary(num: number) {
+function Dec2Binary(num: number) {
     if (num === 0) {
-        return "0"
+        return '0';
     }
 
-    let reserve = "";
+    let reserve = '';
     while (num > 0) {
         reserve = (num % 2) + reserve;
         num = Math.floor(num / 2);
     }
 
-    return reserve
+    return reserve;
 }
 
-function bin2Decimal(num: number) {
+function Bin2Decimal(num: number) {
     let reserve = 0;
     let i = 0;
     while (num > 0) {
-        reserve += ((num % 10) * Math.pow(2, i));
+        reserve += (num % 10) * Math.pow(2, i);
         num = Math.floor(num / 10);
         i++;
     }
-    return reserve
+    return reserve;
 }
 
-
-
-const form = () => {
+const Form: React.FC = () => {
     const [resualt, setResualt] = useState<string>('');
 
     const handleBinary = (e: React.ChangeEvent<HTMLInputElement>) => {
-        let binaryResualt = dec2Binary(parseInt(e.target.value));
+        let binaryResualt = Dec2Binary(parseInt(e.target.value));
         setResualt(binaryResualt);
-    }
+    };
 
     const handleDecimal = (e: React.ChangeEvent<HTMLInputElement>) => {
         const inputValue = e.target.value;
@@ -43,51 +40,50 @@ const form = () => {
         const isValidInput = /^[01]*$/.test(inputValue);
 
         if (isValidInput) {
-            let binaryResult = bin2Decimal(parseInt(inputValue, 2));
+            let binaryResult = Bin2Decimal(parseInt(inputValue, 2));
             setResualt(String(binaryResult));
         } else {
-            alert("invalid Entry");
-            e.target.value = ""
-            setResualt('')
+            alert('Invalid Entry');
+            e.target.value = '';
+            setResualt('');
         }
-    }
-
+    };
 
     return (
         <section className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-green-50 w-4/5 lg:w-6/12 xl:w-4/12 flex flex-col bg-transparent'>
             <div className='flex flex-col my-4'>
-                <h1 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Bin2Dec</h1>
+                <h1 className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Bin2Dec</h1>
                 <input
-                    type="text"
-                    name="binary"
-                    id="binaryInput"
+                    type='text'
+                    name='binary'
+                    id='binaryInput'
                     className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                     onChange={(e) => handleDecimal(e)}
                 />
             </div>
             <div className='flex flex-col my-4'>
-                <h1 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Dec2Bin</h1>
+                <h1 className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Dec2Bin</h1>
                 <input
-                    type="text"
-                    name="decimal"
-                    id="decimalInput"
+                    type='text'
+                    name='decimal'
+                    id='decimalInput'
                     className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                     onChange={(e) => handleBinary(e)}
                 />
             </div>
             <div className='flex flex-col my-4'>
-                <h1 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Resualt</h1>
+                <h1 className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Resualt</h1>
                 <input
-                    type="text"
-                    name="resualt"
-                    id="showResualt"
+                    type='text'
+                    name='resualt'
+                    id='showResualt'
                     className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                     disabled
                     value={String(resualt)}
                 />
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default form
+export default Form;
